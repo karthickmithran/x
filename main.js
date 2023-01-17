d9e9712e8c63c386c9256075d812b9cd69af3d35
karthickmithran <90264421+karthickmithran@users.noreply.github.com>
2023-01-17 17:09:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+img="";
+objects=[];
+status="";
+
+function preload(){
+    
+    loadImage("car.jpeg");
+    loadImage("penbox.jpeg");
+    loadImage("portablecharger1.jpeg");
+    loadImage("portableheadset1.jpeg");
+    loadImage("portableheadset1.jpeg");
+    loadImage("portablelight.jpeg");
+    loadImage("remote.jpeg");
+    loadImage("watch1.jpeg");
+    loadImage("watch2.jpeg");
+    loadImage("waterbottle.jpeg");
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+  }
+
+  function modelLoaded() {
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(img, gotResult);
+  }
+
+  function gotResult(error, results){
+    if(error){
+    console.log(error);
+    }
+    console.log(results);
+    objects = results;
+          }
+    
+          function draw(){
+            image(img , 0, 0, 640, 420);
+            if(status !=""){
+              for(i=0; i<objects.length; i++){
+             
+            document.getElementById("status").innerHTML = "Status:Object Detected";
+        
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+              } 
+          }
+        }
---
+++ watch 1.js
@@ -0,0 +1,41 @@
+img="";
+objects=[];
+status="";
+
+function preload(){
+    loadImage("watch1.jpeg");
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("Status").innerHTML = "Status : Detecting Objects";
+  }
+
+  function modelLoaded() {
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(img, gotResult);
+  }
+
+  function gotResult(error, results){
+    if(error){
+    console.log(error);
+    }
+    console.log(results);
+    objects = results;
+          }
+    
+          function draw(){
+            image(img , 0, 0, 640, 420);
+            
+        
+            fill("#FF0000");
+            
+            text("watch 1", 45, 75);
+            noFill();
+            stroke("#FF0000");
+            rect(30, 60, 450, 350);
+              } 
+          
